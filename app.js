01ec04dc0ab87cab1361bc0385c8043c82ed15b3
Annmariya3820 <mariyadaisy320@gmail.com>
2024-06-14 07:04:36 +0530

    completed

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
+const bcryptjs=require("bcryptjs")
+const jsonwebtoken=require("jsonwebtoken")
 
+
+const {ksrtcmodel} = require("./models/ksrtc")
 
 const app=express()
 app.use(cors())
 app.use(express.json())
 
-app.po
+
+mongoose.connect("mongodb+srv://Annmariyasabu:annmariya@cluster0.gs6ae.mongodb.net/ksrtcdb?retryWrites=true&w=majority&appName=Cluster0")
+
+
+const generatehashedpassword = async(password)=>{
+    const salt=await bcryptjs.genSalt(10)
+    return bcryptjs.hash(password,salt)
+
+}
+
+app.post("/signup",async(req,res)=>{
+    let input=req.body
+    let hashedpassword = await  generatehashedpassword(input.password)
+    console.log(hashedpassword)
+    input.password = hashedpassword
+    let ksrtc=new ksrtcmodel(input)
+    ksrtc.save()
+    res.json({"status":"success"})
+})
+
+
+app.listen(8080,()=>{
+    console.log("server start")
+})
---
+++ models/ksrtc.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+const schema=mongoose.Schema(
+    {
+       "name":{type:String,required:true},
+        "email":{type:String,required:true},
+        "phone":{type:String,required:true},
+        "gender":{type:String,required:true},
+        "password":{type:String,required:true},
+        "confirmpassword":{type:String,required:true}
+    }
+)
+let ksrtcmodel=mongoose.model("ksrtc",schema)
+module.exports={ksrtcmodel}
